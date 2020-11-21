3fadc775c7cebc80ad7709cd7bebc90fd8451bc4
hyo <hyong0616@naver.com>
2020-11-21 20:28:00 +0900

    modify main activity

--- app/src/main/java/com/soongsil/pilatime/MainActivity.java
+++ app/src/main/java/com/soongsil/pilatime/MainActivity.java
@@ -2,7 +2,9 @@ package com.soongsil.pilatime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn1Click(View v) {
+        Intent intent = new Intent(this,AdminCalendarActivity.class);
+        startActivity(intent);
+    }
+
+    public void btn2Click(View v) {
+        Intent intent = new Intent(this,AdminCalendarActivity.class);
+        startActivity(intent);
     }
 }
