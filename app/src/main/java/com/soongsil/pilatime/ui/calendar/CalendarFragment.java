9587a4e91632c68d24fb792a621df42b35b9c6cc
hyo <hyong0616@naver.com>
2020-11-21 23:24:59 +0900

    import Firebase

--- app/src/main/java/com/soongsil/pilatime/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/soongsil/pilatime/ui/calendar/CalendarFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,22 +15,40 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.soongsil.pilatime.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CalendarFragment extends Fragment {
 
     private CalendarViewModel calendarViewModel;
+    public CalendarView calendarView;
+    public TextView dateTextView, contentTextView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        calendarViewModel =
-                ViewModelProviders.of(this).get(CalendarViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-        final TextView textView = root.findViewById(R.id.text_calendar);
-        calendarViewModel.getText().observe(this, new Observer<String>() {
+    //현재 날짜
+    long now = System.currentTimeMillis();
+    Date mDate = new Date(now);
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy/MM/dd");
+    String date = mFormat.format(mDate);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        calendarView = view.findViewById(R.id.admin_calendar);
+        dateTextView = view.findViewById(R.id.textView_date);
+        contentTextView = view.findViewById(R.id.textView_content);
+        dateTextView.setText(date);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                dateTextView.setText(String.format("%d/%d/%d",year,month+1,dayOfMonth));
+                dateTextView.setVisibility(View.VISIBLE);
             }
         });
-        return root;
+
+        return view;
     }
+
+
+
 }
